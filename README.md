2aab9aee8c7fa5609522062e188e990d912cc791
YA <YA-androidapp@users.noreply.github.com>
2020-11-22 15:17:19 +0900

    CRUD を実装

--- README.md
+++ README.md
@@ -19,6 +19,25 @@ $ nano src/main.rs
 $ cargo run
 ```
 
+## CRUD を実装
+
+```sh
+$ nano Cargo.toml
+# [dependencies] に
+# rusqlite = { version = "0.24", features = ["bundled"] }
+# serde = { version = "1.0", features = ["derive"] }
+# を追記
+
+$ cargo add askama r2d2 r2d2_sqlite
+    Updating 'https://github.com/rust-lang/crates.io-index' index
+      Adding askama v0.10.5 to dependencies
+      Adding r2d2 v0.8.9 to dependencies
+      Adding r2d2_sqlite v0.17.0 to dependencies
+$ nano src/main.rs
+$ nano templates/index.html
+$ cargo run
+```
+
 ---
 
 Copyright (c) 2020 YA-androidapp(https://github.com/YA-androidapp) All rights reserved.
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,113 @@
-use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
+use actix_web::{get, http::header, post, web, App, HttpResponse, HttpServer, ResponseError};
+use askama::Template;
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
+use serde::Deserialize;
 use thiserror::Error;
 
+#[derive(Deserialize)]
+struct AddParams {
+    text: String,
+}
+
+#[derive(Deserialize)]
+struct DeleteParams {
+    id: u32,
+}
+struct ItemEntry {
+    id: u32,
+    text: String,
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    entries: Vec<ItemEntry>,
+}
 #[derive(Error, Debug)]
-enum MyError {}
+enum MyError {
+    #[error("Failed to render HTML")]
+    AskamaError(#[from] askama::Error),
+
+    #[error("Failed to get connection")]
+    ConncectionPoolError(#[from] r2d2::Error),
+
+    #[error("Failed SQL execution")]
+    SQLiteError(#[from] rusqlite::Error),
+}
 
 impl ResponseError for MyError {}
 
+#[post("/add")]
+async fn add_item(
+    params: web::Form<AddParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("INSERT INTO item (text) VALUES (?)", &[&params.text])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
+#[post("/delete")]
+async fn delete_item(
+    params: web::Form<DeleteParams>,
+    db: web::Data<r2d2::Pool<SqliteConnectionManager>>,
+) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    conn.execute("DELETE FROM item WHERE id=?", &[params.id])?;
+    Ok(HttpResponse::SeeOther()
+        .header(header::LOCATION, "/")
+        .finish())
+}
+
 #[get("/")]
-async fn index() -> Result<HttpResponse, MyError> {
-    let response_body = "Hello world!";
-    Ok(HttpResponse::Ok().body(response_body))
+async fn index(db: web::Data<Pool<SqliteConnectionManager>>) -> Result<HttpResponse, MyError> {
+    let conn = db.get()?;
+    let mut statement = conn.prepare("SELECT id, text FROM item")?;
+    let rows = statement.query_map(params![], |row| {
+        let id = row.get(0)?;
+        let text = row.get(1)?;
+        Ok(ItemEntry { id, text })
+    })?;
+
+    let mut entries = Vec::new();
+    for row in rows {
+        entries.push(row?);
+    }
+    let html = IndexTemplate { entries };
+    let response_body = html.render()?;
+    Ok(HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response_body))
 }
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
-    HttpServer::new(move || App::new().service(index))
-        .bind("0.0.0.0:80")?
-        .run()
-        .await?;
+    let manager = SqliteConnectionManager::file("item.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS item (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )",
+        params![],
+    )
+    .expect("Failed to create a table `item`.");
+    HttpServer::new(move || {
+        App::new()
+            .service(index)
+            .service(add_item)
+            .service(delete_item)
+            .data(pool.clone())
+    })
+    .bind("0.0.0.0:80")?
+    .run()
+    .await?;
     Ok(())
 }
